db5d7c2e7e4e7f638716dd013d7c94e8e225ff33
Daniel Rios <daniel.rios.ch.int@gmail.com>
2021-10-29 12:34:01 -0500

    Index Update.

--- gateway/src/main/resources/static/js/registro_formulario.js
+++ gateway/src/main/resources/static/js/registro_formulario.js
@@ -1,3 +1,6 @@
+
+
+
 $('#register').submit(function(e) {
 
     console.log("Hacer el Submit");
@@ -6,25 +9,25 @@ $('#register').submit(function(e) {
     registrarNegocio(parametros);
 });
 
-function registrarNegocio(parametros) {
+function registraNegocio() {
 
-	var success = false;
+    var success = false;
+    var parametros = "placeName=" + $('#nombre').val() == "" ? 'El Cabrito X' : $('#nombre').val();
 
-	$.ajax({
-		url: 'http://localhost:4567/create',
-		datatype: 'json',
-		type: 'post',
-		//headers: {'Authorization': 'Basic YnJvd3Nlcjo='},
-		async: false,
-		data: parametros,
-		success: function (data) {
-			console.log(data);
-			success = true;
-		},
-		error: function (error) {
-			console.log(error);
-		}
-	});
-
-	return success;
+    $.ajax({
+        url: 'http://localhost:4567/create',
+        datatype: 'json',
+        type: 'get',
+        data: parametros,
+        contentType: "application/json",
+        success: function(data) {
+            console.log(data);
+            success = true;
+            alert("Ok")
+        },
+        error: function(e) {
+            alert("Ocurrio un error al guardar");
+            console.log(e);
+        }
+    })
 }
